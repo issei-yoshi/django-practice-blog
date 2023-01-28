d8b0b9200f74b2e03f825b767a4a416f59825241
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-28 09:54:35 +0900

    Add: viewsにPostDetailViewクラスを作成

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 
 from blog.models import Post
 
 class PostListView(ListView):
     model = Post
     template_name = "blog/post_list.html"
+
+class PostDetailsView(DeleteView):
+    model = Post
+    template_name = "blog/post_detail.html"
