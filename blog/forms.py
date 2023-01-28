e09f7f353b42e40dc45ed22791b2b18988ae9a8a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-28 16:44:28 +0900

    Add: forms.pyのFormにMetaクラスを定義し、その中に詳細を記載

--- blog/forms.py
+++ blog/forms.py
@@ -2,18 +2,20 @@ from django import forms
 from blog.models import Comment, Reply
 
 class CommentForm(forms.ModelForm):
-    model = Comment
-    fields = ("name", "text")
-    widgets = {
-        'name': forms.TextInput(attrs={'placeholder': '名前'}),
-        'text': forms.Textarea(attrs={'placeholder': 'コメントを入力してください。'})
-    }
+    class Meta:
+      model = Comment
+      fields = ("name", "text")
+      widgets = {
+          'name': forms.TextInput(attrs={'placeholder': '名前'}),
+          'text': forms.Textarea(attrs={'placeholder': 'コメントを入力してください。'})
+      }
 
 
 class ReplyForm(forms.ModelForm):
-    model = Reply
-    fields = ("name", "text")
-    widgets = {
-        'name': forms.TextInput(attrs={'placeholder': '名前'}),
-        'text': forms.Textarea(attrs={'placeholder': '返信を入力してください。'})
-    }
+    class Meta:
+      model = Reply
+      fields = ("name", "text")
+      widgets = {
+          'name': forms.TextInput(attrs={'placeholder': '名前'}),
+          'text': forms.Textarea(attrs={'placeholder': '返信を入力してください。'})
+      }
