c65296cb770f01c1a18d282b74e02921280f0e2d
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-28 15:34:32 +0900

    Add: blog/admin.pyにてPostAdminを作成して管理画面に表示するカラムを変更

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from blog.models import Post, Category, Tag
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=(
+        "title",
+        "category",
+        "created_at",
+        "updated_at",
+        "is_published",
+    )
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
