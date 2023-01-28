698592de2c7c56e928644fed07c562c054f16edf
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-28 11:01:30 +0900

    Add: CategoryPostListViewを作成

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DeleteView
 
-from blog.models import Post
+from blog.models import Post, Category
 
 class PostListView(ListView):
     model = Post
@@ -23,3 +23,16 @@ class PostDetailView(DeleteView):
             return post
         else:
             raise Http404
+
+
+class CategoryPostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+
+    def get_queryset(self):
+        #TOPページでアクセスのあったカテゴリーのURLをslugに代入
+        slug = self.kwargs["slug"]
+        #カテゴリーが存在したら代入、存在しなかったら404エラーを返す
+        self.category = get_object_or_404(Category, slug=slug)
+        #filterを使って取得した記事からカテゴリーごとに絞り込み
+        return super().get_queryset().filter(category=self.category)
