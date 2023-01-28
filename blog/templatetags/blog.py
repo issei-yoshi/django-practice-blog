71e70380b80d5e8496348935ea1bbeaef6c611a7
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-28 15:10:15 +0900

    Add: memo

--- blog/templatetags/blog.py
+++ blog/templatetags/blog.py
@@ -4,7 +4,6 @@ register = template.Library()
 
 @register.simple_tag
 def replace(request, key, value):
-    url_dict = request.GET.copy()
-    url_dict[key] = value
-
-    return url_dict.urlencode()
+    url_dict = request.GET.copy() #query: []をurl_dictに代入
+    url_dict[key] = value #pagenation.htmlで引数にとっている、pageとpage_obj.---を追加
+    return url_dict.urlencode() #urlの形にしてリターン
