23a698534b1cd863798e65439c05cacf1ebc2a2a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-28 11:38:15 +0900

    Add: TagPostListViewのクラスを作成してget_queryメソッド・get_context_dataメソッドをオーバーライド

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DeleteView
 
-from blog.models import Post, Category
+from blog.models import Post, Category, Tag
 
 class PostListView(ListView):
     model = Post
@@ -41,4 +41,20 @@ class CategoryPostListView(ListView):
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs) #object_listなど色々格納されている
         context['category'] = self.category
+        return context
+
+
+class TagPostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        slug = self.kwargs["slug"]
+        self.tag = get_object_or_404(Tag, slug=slug)
+        return super().get_queryset().filter(tag=self.tag)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tag"] = self.tag
         return context
